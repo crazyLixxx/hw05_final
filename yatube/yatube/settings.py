4425b672b239d787e6aed418255ed34df7e8b1c8
crazyLixxx <102381688+crazyLixxx@users.noreply.github.com>
2023-04-23 12:41:24 +0300

    Update settings.py
    sentury integration added to settings

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sentry_sdk
 from dotenv import load_dotenv
+from sentry_sdk.integrations.django import DjangoIntegration
 
 
 load_dotenv()
@@ -160,3 +162,19 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
+
+sentry_sdk.init(
+    dsn="https://9fb5ac8f086c4357a643ffcd0717ac02@o4505062773293056.ingest.sentry.io/4505062778273792",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
